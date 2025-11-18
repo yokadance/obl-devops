4e09e5adc9c8f96938c73807f49ff98bc40de880
Michael Rodriguez <michaelrodriguez@fing.edu.uy>
2025-11-17 22:55:36 -0300

    feat: dev enviroment

---
+++ IaC/terraform/modules/vpc/variables.tf
@@ -0,0 +1,25 @@
+variable "environment" {
+  description = "Environment name"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "availability_zones" {
+  description = "List of availability zones"
+  type        = list(string)
+}
+
+variable "public_subnet_cidrs" {
+  description = "CIDR blocks for public subnets"
+  type        = list(string)
+}
+
+variable "private_subnet_cidrs" {
+  description = "CIDR blocks for private subnets"
+  type        = list(string)
+}
